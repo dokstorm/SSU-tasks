a5ed8a5e98dda1ac055e0fd4c166ecb82bd9c540
Dmitriy Gatilov <gatilovds@gmail.com>
2018-11-19 01:29:47 +0400

    update

--- password_encryption/src/Source.java
+++ password_encryption/src/Source.java
@@ -1,30 +1,37 @@
 import java.io.File;
-import java.util.Arrays;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Source {
-    public static void encryptDirectory(String path, String password) {
+    public static void encryptDirectory(String path, String password) throws IOException {
         File dir = new File(path);
-        
+
+        File archive = new File(dir.getCanonicalPath() + ".arch");
+        archive.createNewFile();
+        for (File item : dir.listFiles()) {
+
+        }
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Введите путь к шифруемой директории:");
         Scanner in = new Scanner(System.in);
         //String path = in.nextLine();
-        String path = "C:/Users/Dmitriy/Desktop/test1";
-        System.out.println("Введите пароль:");
+        String path = "C:/Users/Dmitriy/Desktop/test1/test";
+        /*System.out.println("Введите пароль:");
         String pass1 = in.nextLine();
         System.out.println("Повторите пароль:");
         String pass2 = in.nextLine();
         if (!pass1.equals(pass2)) {
             System.out.println("Пароли не совпадают");
         }
-        else {
+        else {*/
+        String pass2 = "123";
+
             encryptDirectory(path, pass2);
             System.out.println("Директория успешно зашифрована");
-        }
+        //}
 
 
 
